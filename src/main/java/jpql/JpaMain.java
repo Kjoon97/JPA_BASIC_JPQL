5171a227eb0c9c41354b127d8cd205685a2504da
joonhyuck <jh83370@naver.com>
2022-04-03 03:31:08 +0900

    프로젝션(SELECT)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,14 +19,28 @@ public class JpaMain {
             member.setUsername("철수");
             member.setAge(10);
             em.persist(member);
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            Query query2 = em.createQuery("select m.username, m.age from Member m");
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username","철수")
-                    .getSingleResult();
-            System.out.println("singleResult = " + result.getUsername());
+            //프로젝션 여러 값 조회하는 법 1.Query 타입으로 조회
+            List resultList = em.createQuery("select m.username,m.age from Member m").getResultList();
+            Object o = resultList.get(0);
+            Object[] QueryResult = (Object[]) o;
+            System.out.println("QueryResult = " + QueryResult[0]);
+            System.out.println("QueryResult = " + QueryResult[1]);
 
+            //프로젝션 여러 값 조회하는 법 2. Object[] 타입으로 조회
+            List<Object[]> resultList1 = em.createQuery("select m.username,m.age from Member m").getResultList();
+            Object[] typeresult = resultList1.get(0);
+            System.out.println("typeresult = " + typeresult[0]);
+            System.out.println("typeresult = " + typeresult[1]);
+
+
+            //프로젝션 여러 값 조회하는 법 3. (new 명령어로 조회하기)  ->이 방법이 젤 깔끔함.
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
             tx.commit();
 
         }catch (Exception e){
