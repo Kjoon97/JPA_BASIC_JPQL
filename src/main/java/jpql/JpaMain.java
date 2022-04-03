18004414127428a6ace151062f12e6e95244f92b
joonhyuck <jh83370@naver.com>
2022-04-04 01:34:00 +0900

    벌크 연산- 한 건이 아닌 여러개를 한꺼번에 update, delete

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -24,29 +24,36 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
+            //flush 될 때(DB에 반영될때)는 커밋하거나 직접 쿼리를 호출할 때.
+            //벌크 연산을 통해 모든 회원의 나이를 20살로 맞추겠다.
+            int resultCount = em.createQuery("update Member m set m.age=20")   //업데이트 된 것들 개수 반환
+                    .executeUpdate();
+
+            //벌크 연산을 위해 초기화하고 다시 불러와야
             em.clear();
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
-
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            //벌크 연산이 디비에는 반영이되어 회원 나이가 20살로 조회 되지만 영속성 컨텍스트에서는 반영 안되므로 다음과같은 print에서는 0살로 출력된다. 그래서 em.clear해줘야함.
+            System.out.println("member1.age" + member1.getAge());
+            System.out.println("member2.age" + member2.getAge());
+            System.out.println("member3.age" + member3.getAge());
 
             tx.commit();
 
