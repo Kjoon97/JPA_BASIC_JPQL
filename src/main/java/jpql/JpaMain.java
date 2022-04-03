83992a303849ea809a36b9a0a1c6b1e9253a9d4b
joonhyuck <jh83370@naver.com>
2022-04-03 15:43:26 +0900

    조건식(CASE 등등)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,8 +19,9 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("관리자");
             member.setAge(10);
+            member.setType(MemberType.USER);
 
             member.setTeam(team);
             em.persist(member);
@@ -28,10 +29,23 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m inner join m.team t";
+            //기본 case식(이런 것들 다 querydsl로 하기 편함)
+            String query ="select "+
+                    "case when m.age <=10 then '학생요금' "+
+                    "     when m.age >=60 then '경로요금' "+
+                    "     else '일반요금' "+
+                    "end "+
+                          "from Member m";
+            //COALESCE - 하나씩 조회해서 null 아니면 반환
+            String query2 = "select coalesce(m.username, '이름 없는 회원') from Member m";
 
-            List<Member> result = em.createQuery(query, Member.class)
-                    .getResultList();
+            //NULLIF : 사용자 이름이 관리자이면 null반환, 나머지는 본인의 이름 반환
+            String query3 = "select NULLIF(m.username, '관리자') from Member m";
+            List<String> resultList = em.createQuery(query3, String.class).getResultList();
+            for (String s : resultList) {
+                System.out.println("s = " + s);
+            }
+
             tx.commit();
 
         }catch (Exception e){
