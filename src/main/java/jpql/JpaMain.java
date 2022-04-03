ede7b6cc5423cfdd3c5dedcc23a6127aaf42a9a4
joonhyuck <jh83370@naver.com>
2022-04-03 21:24:56 +0900

    패치 조인2 - 특징과 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -40,24 +40,21 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-//            String query = "select m From Member m join fetch m.team"; //->member를 조회하는데 join할때 team을 한번에 다 가져오는 것.(지연 로딩을 해도 패치 조인이 더 우선으로 먼저 세팅됨.)
-//
-//            List<Member> resultList = em.createQuery(query, Member.class).getResultList();
-//            for (Member member : resultList) {
-//                System.out.println("member = " + member.getUsername() + ","+ member.getTeam().getName());
-//                //이때 member.getTeam()에서 team은 프록시가 아님.( fetch조인을 통해서 이미 member랑 팀을 조인해서 한꺼번에 다 select함 이미 실제 team이 영속성 컨텍스트에 다 올라가있음.)
-//            }
-            //일대 다 패치 조인과 distinct
-            String query = "select distinct t From Team t join fetch t.members";
-            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+            String query ="select t from Team t";
+
+            List<Team> resultList = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
+
             System.out.println("resultList = " + resultList.size());
+
             for (Team team : resultList) {
-                System.out.println("team = " + team.getName() +"|members: "+ team.getMembers().size());
-                for( Member member : team.getMembers()){
+                System.out.println("team = " + team.getName() + team.getMembers());
+                for (Member member : team.getMembers()) {
                     System.out.println("member = " + member);
                 }
             }
-
 
             tx.commit();
 
